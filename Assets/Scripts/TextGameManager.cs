74b0cce21c8d4411acbe3d392310ad5d6a0d692d
ChrisIvenDC <134356337+ChrisIvenDC@users.noreply.github.com>
2023-06-06 14:33:38 +0800

    Added UI

--- Assets/Scripts/TextGameManager.cs
+++ Assets/Scripts/TextGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEditor;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -10,12 +11,15 @@ public class NewBehaviourScript : MonoBehaviour
     public TextMeshProUGUI storytextMeshPro, hptextMeshPro, statextmeshPro;
     public string storyText;
     public int HPVal, StaVal;
-    public GameObject Level1_Object, PostThornyPath_Object, PostGravelPath_Object;
+    public GameObject 
+        UIStatsimg,
+        MainMenu_Object, Level1_Object, PostThornyPath_Object, PostGravelPath_Object,
+        Thorn_Path, Gravel_Path;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        MainMenu_Object.SetActive(true);
     }
 
     // Update is called once per frame
@@ -23,11 +27,27 @@ public class NewBehaviourScript : MonoBehaviour
     {
         storytextMeshPro.text = storyText;
         hptextMeshPro.text = HPVal.ToString();
-        statextmeshPro.text = StaVal.ToString();    
+        statextmeshPro.text = StaVal.ToString();
     }
     
     
-    //LEVEL1
+    //TITLE MENU
+    public void Menu()
+    {
+        MainMenu_Object.SetActive(true);  
+        Level1_Object.SetActive(false);
+        UIStatsimg.SetActive(false);
+    }
+
+
+    //START
+    public void GameStart()
+    {
+        UIStatsimg.SetActive(true);
+        Level1_Object.SetActive(true);
+        MainMenu_Object.SetActive(false);
+    }
+    //LEVELS
     public void ThornyPath()
     {
         storyText = "You managed to get pass through despite having cuts and bruises all over your body, You also found a Steel Sword on the way! Now there's a plant monster in front of you, What do you do?";
@@ -43,19 +63,55 @@ public class NewBehaviourScript : MonoBehaviour
 
         Level1_Object.SetActive(false);
         StaVal = StaVal - 1;
-        PostThornyPath_Object.SetActive(true);
+        PostGravelPath_Object.SetActive(true);
     }
 
     public void HugeFootPrints()
     {
-        storyText = "While following the path, the footprint stopped midway. You heard a low growl. A huge mosnter ate you. You died.";
+        storyText = "While following the path, the footprint stopped midway. You heard a low growl. A huge mosnter ate you. \n \n \n Obvious Ending: Try not to be too curious" ;
         HPVal = HPVal - 10;
         StaVal = StaVal - 10;
 
         Level1_Object.SetActive(false);
     }
 
-    //LEVEL 2
+    //Thorn Route
 
+    public void Fight_Thorn()
+    {
+        storyText = "You managed to kill the plant monster! it dropped tons of gold! you continued your path and managed to escape the forest! \n \n \n Best Ending: Loot! Loot! but barely alive";
+        HPVal = HPVal - 4;
+        StaVal = StaVal - 6;
+        PostThornyPath_Object.SetActive(false);
+    }
 
+    public void Escape_Thorn()
+    {
+        storyText = "You Escaped the Plant! And managed to escape the forest with a shiny steel sword!  \n \n \n Normal Ending: the sword was shiny!";
+        StaVal = StaVal - 3;
+        PostThornyPath_Object.SetActive(false);
+    }
+
+    //Gravel Route
+    public void Fight_Gravel()
+    {
+        storyText = "You tried fighting the plant wiht your bare hands! It Impaled you with its thorns, you died. \n \n \n Noob Ending: THE VEGTABLE WAS HUGE!";
+        HPVal = HPVal - 10;
+        StaVal = StaVal - 9;
+        PostGravelPath_Object.SetActive(false);
+
+    }
+
+    public void Escape_Gravel()
+    {
+        storyText = "The pland didnt notice you, and you managed to escape. \n \n \n Boring Ending: wow! You escaped... Nice...";
+        StaVal = StaVal - 2;
+        PostGravelPath_Object.SetActive(false);
+
+    }
+    //EXIT 
+    public void Close()
+    {
+        Application.Quit();
+    }
 }
